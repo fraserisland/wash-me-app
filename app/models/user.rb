f7351e0f7c07bc6524656953781a1d0066df2de6
Fraser Solomon <fm_solomon@hotmail.com>
2018-05-03 15:44:05 +1000

    embed links on map icons, and user profil thumbnails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :avatar, AvatarUploader
+  
   ratyrate_rater
+
   has_many :washers
 
   devise :database_authenticatable, :registerable,
